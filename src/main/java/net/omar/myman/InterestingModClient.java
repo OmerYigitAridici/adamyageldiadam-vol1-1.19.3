757212d44229cb9fff3afa4a2cd9655c9d086743
soupperman <84670954+OmerYigitAridici@users.noreply.github.com>
2023-01-31 15:19:13 +0300

    Initial commit

---
+++ src/main/java/net/omar/myman/InterestingModClient.java
@@ -0,0 +1,10 @@
+package net.omar.myman;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InterestingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
